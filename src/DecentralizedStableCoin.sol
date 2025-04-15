214ab0d4df41faf5e3c99f872e7915a45f5df90d
txusif <txusif@gmail.com>
2025-04-15 23:46:30 +0530

    add burn and mint function in DecentralizedStableCoin

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.18;
 
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title Decentralized Stable Coin
@@ -13,7 +14,36 @@ import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensio
  *
  * This is the contract meant to be governed by the DSC Engine. This contract is just the ERC20 implementation of our stable coin system.
  */
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__BurnAmountZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__MintToZeroAddress();
+    error DecentralizedStableCoin__MintAmountZero();
 
-contract DecentralizedStableCoin is ERC20Burnable {
     constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+
+        if (_amount < 0) {
+            revert DecentralizedStableCoin__BurnAmountZero();
+        }
+
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__MintToZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MintAmountZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
 }
